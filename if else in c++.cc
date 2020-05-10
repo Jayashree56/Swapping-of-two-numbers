16bf1cf2bc0a6d1ef5c125f81e6fc7e027411600
Jayashree56 <63342845+Jayashree56@users.noreply.github.com>
2020-05-10 13:55:57 +0530

    this is a c++ program for if else statement
    this program code for eligibility of voting

---
+++ if else in c++.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int age;
+   cout<<"Enter the age of the user:";
+   cin>>age;
+   if(age>=18)
+   {
+      cout<<"\nYou are eligibile to vote!"<<"\nVote Wisely.";
+      }
+   else
+   {
+   cout<<"\n Sorry,You are not eligible to vote!<<"\nTill then study wisely!";
+   }
+   retrn 0;
+   }
+   
