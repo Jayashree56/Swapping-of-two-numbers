c4a84853f89e9b6a787c5185cf0af5bf698bc135
Jayashree56 <63342845+Jayashree56@users.noreply.github.com>
2020-05-11 19:16:55 +0530

    arrays in c++

---
+++ arrays in c++.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int data[5];
+ cout<<"Enter elements:";
+ for(i=0;i<5;++i)
+cin>>data[i];
+cout<<"You entered:";
+for(i=0;i<5;++i)
+cout<<end1<<*(data + i);
+   return 0;
+   }
